96988f64f456542518f197d29613695584300731
tanliubin <1406901724@qq.com>
2019-03-29 11:33:03 +0800

    init

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const webpackConfigBase = require('./webpack.base.conf');
+const webpackConfigDev = {
+    mode: 'development', // 通过 mode 声明开发环境
+
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        // 打包多出口文件
+        filename: './js/[name].bundle.js'
+    },
+
+    plugins: [
+        //热更新
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.BASE_URL': '\"' + process.env.BASE_URL + '\"'
+        })
+    ],
+
+    devtool: "source-map", // 开启调试模式
+
+    devServer: {
+        contentBase: path.join(__dirname, "../src"),
+        publicPath: '/',
+        historyApiFallback: true,
+        host: "127.0.0.1",
+        port: "8080",
+        overlay: true, // 浏览器页面上显示错误
+        open: true, // 开启浏览器
+        stats: "errors-only", //stats: "errors-only"表示只打印错误：
+        hot: true, // 开启热更新
+        //服务器代理配置项
+        proxy: {
+            '/testing/*': {
+                target: 'https://www.baidu.com',
+                secure: true,
+                changeOrigin: true
+            }
+        }
+    },
+}
+module.exports = merge(webpackConfigBase, webpackConfigDev);
---
+++ build/webpack.rules.conf.js
@@ -0,0 +1,64 @@
+const extractTextPlugin = require("extract-text-webpack-plugin");
+const rules = [
+    {
+        test: /\.(css|scss|sass)$/,
+        // 区别开发环境和生成环境
+        use: process.env.NODE_ENV === "development" ? ["style-loader", "css-loader", "sass-loader", "postcss-loader"] : extractTextPlugin.extract({
+            fallback: "style-loader",
+            use: ["css-loader", "sass-loader", "postcss-loader"],
+            publicPath: "../"
+        })
+    },
+    {
+        test: /\.js$/,
+        use: [{
+            loader: "babel-loader"
+        }],
+        // 不检查node_modules下的js文件
+        // exclude: "/node_modules/"
+    }, {
+        test: /\.(png|jpg|gif)$/,
+        use: [{
+            // 需要下载file-loader和url-loader
+            loader: "url-loader",
+            options: {
+                limit: 5 * 1024, //小于这个时将会已base64位图片打包处理
+                // 图片文件输出的文件夹
+                outputPath: "images"
+            }
+        }]
+    },
+    {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+            limit: 10000,
+        }
+    },
+    {
+        test: /\.html$/,
+        // html中的img标签
+        use: {
+            loader: 'html-loader?config=raw-loader',
+            options: {
+                attrs: ['img:src']
+            }
+        }
+    }, {
+        test: /\.less$/,
+        // 三个loader的顺序不能变
+        // 不分离的写法
+        // use: ["style-loader", "css-loader", "less-loader"]
+        // 区别开发环境和生成环境
+        use: process.env.NODE_ENV === "development" ? ["style-loader", "css-loader", "less-loader"] : extractTextPlugin.extract({
+            fallback: "style-loader",
+            use: ["css-loader", "less-loader"],
+            // css中的基础路径
+            publicPath: "../dist/"
+        })
+    }, {
+        test: require.resolve('zepto'),
+        loader: 'exports-loader?window.Zepto!script-loader'
+    }
+];
+module.exports = rules;
